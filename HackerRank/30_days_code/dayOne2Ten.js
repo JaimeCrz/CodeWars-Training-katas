fdbc2b67fe8c24a43c1193248b065ef48c51c7b5
Jaime <jaime.cruz.hostalot@gmail.com>
2020-05-26 23:32:09 +0200

    day 1 to 2

---
+++ HackerRank/30_days_code/dayOne2Ten.js
@@ -0,0 +1,31 @@
+// Day one problem, Hello world function. ( don't forget to use JS)
+function processData(inputString) {
+  console.log("Hello, World.");
+  console.log(inputString)
+}
+
+//Comments: A good start for those starting to code, with good resources and tutorials.
+
+
+// Day two, Data types.
+// Declare second integer, double, and String variables.
+let secondInt, secondDouble, secondString
+
+// Read and save an integer, double, and String to your variables.
+secondInt = parseInt(readLine())
+secondDouble = parseFloat(readLine())
+secondString = readLine()
+let sum = d + secondDouble
+
+// Print the sum of both integer variables on a new line.
+console.log(i + secondInt)
+
+// Print the sum of the double variables on a new line.
+console.log(sum.toFixed(1))
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + secondString)
+
+//Comments: Tutorial information it is a little ambiguous.
+// The level of it could be a little above for those starting (if it's your 2 day) but a good challenge.
