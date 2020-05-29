b33880fb23a32234daef77ba1db9680f1c948d29
Jaime <jaime.cruz.hostalot@gmail.com>
2020-05-29 23:52:45 +0200

    [CodInGame] conditionals

---
+++ CodInGame/Practice/conditions.js
@@ -0,0 +1,16 @@
+// Training conditionals.
+
+while (true) {
+  let enemy1 = readline();
+  let dist1 = parseInt(readline());
+  let enemy2 = readline(); 
+  let dist2 = parseInt(readline()); 
+
+  if (dist1 < dist2) {
+      console.log(enemy1);
+  } else {
+      console.log(enemy2);
+  }
+}
+
+//Comments: quite nice site, good way of training if you like more visualisations as well as several programing languages that you can choose from like codewars.
