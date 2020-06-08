60b4f1afa4ac2eacf17a64f0c68d1e7c920aa35b
Jaime <jaime.cruz.hostalot@gmail.com>
2020-06-08 23:23:22 +0200

    [codewars] find the odd int

---
+++ CodeWars/JavaScript/findTheOddInt.js
@@ -0,0 +1,16 @@
+// Given an array, find the integer that appears an odd number of times.
+
+function findOdd(A) {
+  let count = 0;
+ for(let i = 0; i < A.length; i++) {
+   for(let j = 0; j < A.length; j++) {
+         if(A[i] == A[j]) {
+           count++;
+         }
+       }
+   if (count % 2 !== 0) {
+       return A[i];
+   }
+ }
+ count = 0;
+}
