3cacbc287a1b1335ffc3c82b5566d823321c7d91
Jaime <jaime.cruz.hostalot@gmail.com>
2020-06-22 23:59:37 +0200

    [codewars] alternate capitalization comments

--- CodeWars/JavaScript/alternateCapitalization.js
+++ CodeWars/JavaScript/alternateCapitalization.js
@@ -1,6 +1,8 @@
-
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
 
 function capitalize(s) {
   return [s.split('').map((item, idx) => { return idx % 2 === 0 ? item.toUpperCase() : item }).join(''),
   s.split('').map((item, idx) => { return idx % 2 === 0 ? item : item.toUpperCase() }).join('')];
 };
+
+//comment: a serie of usage for split methode
